0c8f74f0ffba65e1b5fd99478471164b733a3eff
anushil <anushil.k@collpoll.com>
2019-12-25 20:07:27 +0530

    Added Question for Day 1 , gitignore for .class and launch.json file that need modification

---
+++ Day_1.java
@@ -0,0 +1,13 @@
+/*
+Google:
+Given a list of numbers and a number k, 
+return whether any two numbers from the list add up to k.
+
+For example, 
+given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+*/
+public class Day_1 {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
